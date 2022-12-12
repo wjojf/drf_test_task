deb318b7becedf95dbaac72463d8e5c3f8ce9625
wjojf <STPKBFF@yandex.ru>
2022-12-12 23:33:49 +0500

    Add FilteredListMixin

--- app/api/views.py
+++ app/api/views.py
@@ -1,9 +1,17 @@
+from rest_framework.response import Response
 from rest_framework import viewsets
 from core.models import Item
 from core.serializers import ItemSerializer
+from core.api_utils import FilteredListMixin
 
 
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
 
+
+    def list(self, request, *args, **kwargs):
+        series_qs, qs_status = FilteredListMixin.filter_qs(request, self.queryset)
+        data = self.serializer_class(series_qs, many=True).data
+        
+        return Response(data, status=qs_status)
--- app/core/api_utils.py
+++ app/core/api_utils.py
@@ -1,5 +1,31 @@
+from rest_framework import status
 from core.models import Item
+import json
 import os
+
+
+class FilteredListMixin(object):
+
+    LOOKUP_PARAM = '_filter'
+
+    class Meta:
+        abstract = True
+
+    @classmethod
+    def filter_qs(cls, request, qs):
+        try:
+            body = json.loads(request.body.strip())
+            print(body)
+        except Exception:
+            body = {}
+
+        if FilteredListMixin.LOOKUP_PARAM not in body:
+            return qs, status.HTTP_200_OK
+
+        try:
+            return qs.filter(**body[FilteredListMixin.LOOKUP_PARAM]), status.HTTP_200_OK
+        except Exception:
+            return None, status.HTTP_400_BAD_REQUEST
 
 
 def get_filepath_and_extension(filepath):
